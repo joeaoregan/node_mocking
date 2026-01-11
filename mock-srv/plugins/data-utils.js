6f5f0dc43e58bd3e60d44067a4af0057b11f0e81
joeaoregan <joe.oregan@gmail.com>
2026-01-11 12:11:40 +0000

    current & realtime orders

--- mock-srv/plugins/data-utils.js
+++ mock-srv/plugins/data-utils.js
@@ -1,26 +1,19 @@
 "use strict";
 
+const { promisify } = require("util");
 const fp = require("fastify-plugin");
+const timeout = promisify(setTimeout);
+
+const orders = {
+  A1: { total: 3 },
+  A2: { total: 7 },
+  B1: { total: 101 },
+};
 
 const catToPrefix = {
   electronics: "A",
   confectionery: "B",
 };
-
-const calculateID = (idPrefix, data) => {
-  const sorted = [...new Set(data.map(({ id }) => id))];
-  const next = Number(sorted.pop().slice(1)) + 1;
-  return `${idPrefix}${next}`;
-};
-
-module.exports = fp(async function (fastify, opts) {
-  fastify.decorateRequest("mockDataInsert", function insert(category, data) {
-    const request = this;
-    const idPrefix = catToPrefix[category];
-    const id = calculateID(idPrefix, data);
-    data.push({ id, ...request.body });
-  });
-});
 
 async function* realtimeOrdersSimulator() {
   const ids = Object.keys(orders);
@@ -42,3 +35,21 @@ function* currentOrders(category) {
     yield JSON.stringify({ id, ...orders[id] });
   }
 }
+
+const calculateID = (idPrefix, data) => {
+  const sorted = [...new Set(data.map(({ id }) => id))];
+  const next = Number(sorted.pop().slice(1)) + 1;
+  return `${idPrefix}${next}`;
+};
+
+module.exports = fp(async function (fastify, opts) {
+  fastify.decorate("currentOrders", currentOrders);
+  fastify.decorate("realtimeOrders", realtimeOrdersSimulator);
+  fastify.decorateRequest("mockDataInsert", function insert(category, data) {
+    const request = this;
+    const idPrefix = catToPrefix[category];
+    const id = calculateID(idPrefix, data);
+    orders[id] = { total: 0 };
+    data.push({ id, ...request.body });
+  });
+});
